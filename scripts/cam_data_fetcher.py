c9af1e76b2ffefc6a483bcf37ff2f37efadc6203
mediadhBani <mohamed-iadh.bani@netc.fr>
2021-06-18 15:16:47 +0200

    Codage de l'asservissement visuel. Mouvement saccadé.

--- scripts/cam_data_fetcher.py
+++ scripts/cam_data_fetcher.py
@@ -1,26 +1,51 @@
 import cv2
+import numpy as np
+from send_pos import Kossel
 
-NOIR = ((0, 0, 0), (255, 255, 25))
-H, W = 480, 640  # TODO
+BLACK = ((0, 0, 0), (255, 255, 25))
+RED = (0, 63, 63), (10, 255, 255)
+BLUE = (150, 63, 63), (190, 255, 255)
+H, W = 480, 640
+# R = np.array([[-.5, -np.sqrt(3)/2], [-.5, np.sqrt(3)/2]])
+R = np.full((2, 2), -1/np.sqrt(2)); R[0, 0] *= -1
+
+CLR = RED
 
 vid = cv2.VideoCapture(1)  # caméra secondaire (usb)
+
+delta = Kossel('COM7', 322.5, 150, 75, 80)
+delta.goto(z=100, pause=2)
 
 while True:
     _, frame = vid.read()
 
     pov = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)  # conversion BGR vers HSV
-    pov = cv2.inRange(pov, *NOIR)                 # binarisation
-    pov = cv2.bitwise_not(pov)                    # inversion binarisation
+    pov = cv2.inRange(pov, *CLR)                  # binarisation
+    # pov = cv2.bitwise_not(pov)                  # inversion binarisation
 
     mmt = cv2.moments(pov)
 
-    cx, cy = mmt['m10'] / mmt['m00'], mmt['m01'] / mmt['m00']
+    if mmt['m00']:
+        cx, cy = mmt['m10'] / mmt['m00'], mmt['m01'] / mmt['m00']
+        print(cx, cy)
+        cv2.ellipse(pov, ((cx, cy), (30, 30), 0), (255, 0, 0), 2)
 
-    cv2.ellipse(pov, ((cx, cy), (50, 50), 0), (127, 127, 127), 2)
+        # centrage
+        err = (cx * 2/W - 1, cy * 2/H - 1)
+        err = R @ err * 2
+        # ex, ey = ex - 10, ey - 10
+        # print(err)
+        delta.move(*err)
 
+    cv2.ellipse(pov, ((W/2, H/2), (30, 30), 0), (192, 192, 192), 2)
     cv2.imshow("Capture", pov)
 
-    if cv2.waitKey(40) & 0xFF == ord('q'):
+    if cv2.waitKey(20) & 0xFF == ord('q'):
         vid.release()
         cv2.destroyAllWindows()
         break
+
+    elif cv2.waitKey(20) & 0xFF == ord('h'):
+        delta.home()
+        delta.goto(z=100, pause=3)
+        pass
--- scripts/send_pos.py
+++ scripts/send_pos.py
@@ -1,46 +1,42 @@
-from typing_extensions import ParamSpecKwargs
 import serial as serial
 import click
 import time
 
 class Kossel:
-    def __init__(self, port: str, height: float):
-        self._x, self._y, self._z = 0, 0, height
-        self._h = [0, 0, height]  # position home
-        self._s = serial.Serial(port, 250000)
+    def __init__(self, port: str, height: float, forearm: float,
+                 in_radius: float, out_radius: float):
 
-        self.home()
+        self._x, self._y, self._z = 0, 0, height
+
+        self._s = serial.Serial(port, 250000)
+        self.h = (0, 0, height)
+
+        self._s.write(b'G90\n')
+        self.home() 
+
+        time.sleep(2)
 
     def home(self):
         self._s.write(b'G28\n')  # initialisation de la position
-        time.sleep(1)
-        self._x, self._y, self._z = self._h
+        self._x, self._y, self._z = self.h
+        time.sleep(2)
 
-    def goto_ar(self, d1: float=0, d2: float=0, d3: float=0):
-        pass
+    def goto(self, x: float=0, y: float=0, z: float=0, pause: float=0):
+        if self.can_reach(x, y, z):
+            self._x, self._y , self._z = x, y, z
+            self._s.write(bytes(f'G1 X{x} Y{y} Z{z}\n', encoding='utf-8'))
 
-    def goto_op(self, x: float=0, y: float=0, z: float=0):
-        if 0 < z <= self._h:
-            self._s.write(bytes(f'G90\nG1 X{x} Y{y} Z{z}\n', encoding='utf-8'))
-            self._x, self._y, self._z = x, y, z
-
+            time.sleep(pause)
         else:
-            print(f"Position inatteignable : X{x} Y{y} Z{z}")
-
-    def move_ar(self, d1: float=0, d2: float=0, d3: float=0):
-        pass
-
-    def move_op(self, x: float=0, y: float=0, z: float=0):
-        if self.is_within_range_op(self._x + x, self._y + y, self._z + z):
-            self._s.write(bytes(f'G91\nG1 X{x} Y{y} Z{z}\n', encoding='utf-8'))
-        else:
-            print(f"Mouvement interdit : X{x} Y{y} Z{z}")
-
-    def is_within_range_op(self, x: float, y: float, z: float):
-        pass
+            print(f"WRN Can't reach target : X{x} Y{y} Z{z}")
     
-    def is_within_range_ar(self, d1: float, d2: float, d3: float):
-        pass
+    def move(self, x: float=0, y: float=0, z: float=0, pause: float=0):
+        self.goto(self._x + x, self._y + y, self._z + z, pause)
+
+    def can_reach(self, x: float, y: float, z: float):
+        # if self.L**2 - (self.r - self.R + x)**2 - 
+        return True
+        
 
 if __name__ == '__main__':
     try:
@@ -81,8 +77,6 @@ if __name__ == '__main__':
 
         elif c == 'q':  # quitter
             break
-
-        time.sleep(.2)
 
     ser.write(b'G90\n')  # coordonnees absolues
     time.sleep(1)
